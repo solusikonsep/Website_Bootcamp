61903f17878b5766598fe642f7b3eff65a02f26a
Alam Wibowo <alamhafidz@gmail.com>
2024-10-06 21:13:04 +0700

    update yu

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require("@snipkode/server");
 
 ROUTE("GET /", function(){
-    this.view("home")
+    this.view("pages/landingpage")
 });
+
+ROUTE("GET /todo", function(){
+    this.view("pages/todo");
+});
+
+ROUTE("GET /dashboard", function(){
+    this.view("pages/dashboard");
+});
+
+
 
 HTTP("debug", { port: 5000 });
